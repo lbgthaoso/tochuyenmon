bd6fbef1da0f5735385536cbcc14d7ecf4e6c662
lbgthaoso <lbgthaoso@gmail.com>
2026-09-25 11:55:47 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base:'/to5/',
+    base:'/tochuyenmon/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
